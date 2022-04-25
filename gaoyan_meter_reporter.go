32d40e58c33137aefbd3f72379782f3146e3e63c
unknown <angela.monster@gmail.com>
2022-04-25 23:47:28 +0800

    add pool

--- gaoyan_meter_reporter.go
+++ gaoyan_meter_reporter.go
@@ -5,7 +5,6 @@ package main
 import (
 	"crypto/tls"
 	"encoding/json"
-	"flag"
 	"log"
 	"os"
 	"os/signal"
@@ -27,17 +26,19 @@ func do_job(c mqtt.Client) {
 		if err != nil {
 			log.Printf("Read error: %s\n", err)
 		} else {
-			if false == meter.ConfigSent {
-				log.Println("Publish Config")
-				meter.PublishConfig(c)
-				meter.ConfigSent = true
-			}
 			//log.Print(json_string)
 
 			json_bytes, err := json.Marshal(info)
 			if err != nil {
 				log.Println(err)
 			} else {
+
+				if false == meter.ConfigSent {
+					log.Println("Publish Config")
+					meter.PublishConfig(c)
+					meter.ConfigSent = true
+				}
+
 				meter.PublishData(c, string(json_bytes))
 				log.Printf("P:%f\n", info.P)
 			}
@@ -49,65 +50,20 @@ func do_job(c mqtt.Client) {
 var mqtt_status = 0
 
 func init_mqtt() mqtt.Client {
-	// for i := 0; i < 5; i++ {
-	// 	text := fmt.Sprintf("this is msg #%d!", i)
-	// 	token := c.Publish("go-mqtt/sample", 0, false, text)
-	// 	token.Wait()
-	// }
-
-	//fmt.Printf("wait -- ")
 	hostname, _ := os.Hostname()
 
-	//mqtt.DEBUG = log.New(os.Stdout, "", 0)
-	//mqtt.ERROR = log.New(os.Stdout, "", 0)
+	connOpts := mqtt.NewClientOptions()
+	connOpts.AddBroker("tls://w.wiin.win:1884")
+	connOpts.SetClientID(hostname + "-meter-reporter")
+	connOpts.SetCleanSession(true)
+	connOpts.SetUsername("mao")
+	connOpts.SetPassword("linmao8888")
 
-	// opts := mqtt.NewClientOptions().AddBroker("tcp://iot.wiin.win:11883").SetClientID("Status-" + hostname)
-	// opts.SetKeepAlive(2 * time.Second)
-	// //opts.SetDefaultPublishHandler(f)
-	// opts.SetPingTimeout(1 * time.Second)
-	// opts.SetUsername("mao")
-	// opts.SetPassword("linmao8888")
-
-	server := flag.String("server", "tls://w.wiin.win:1884", "The full url of the MQTT server to connect to ex: tls://w.wiin.win:1884")
-	//topic := flag.String("topic", "#", "Topic to subscribe to")
-	//qos := flag.Int("qos", 0, "The QoS to subscribe to messages at")
-	//clientid := flag.String("clientid", hostname+strconv.Itoa(time.Now().Second()), "A clientid for the connection")
-	clientid := flag.String("clientid", hostname+"-power-meter", "A clientid for the connection")
-	username := flag.String("username", "mao", "A username to authenticate to the MQTT server")
-	password := flag.String("password", "linmao8888", "Password to match username")
-	flag.Parse()
-
-	connOpts := mqtt.NewClientOptions().AddBroker(*server).SetClientID(*clientid).SetCleanSession(true)
-	if *username != "" {
-		connOpts.SetUsername(*username)
-		if *password != "" {
-			connOpts.SetPassword(*password)
-		}
-	}
 	tlsConfig := &tls.Config{InsecureSkipVerify: true, ClientAuth: tls.NoClientCert}
 	connOpts.SetTLSConfig(tlsConfig)
 
 	connOpts.OnConnect = func(c mqtt.Client) {
 		log.Println("mqtt OnConnect")
-		// topics := map[string]byte{"haworkshopyc1/sensor/w0004/state": 0,
-		// 	"haworkshopyc1/sensor/w0005/state":         0,
-		// 	"haworkshopyc1/sensor/w0007/state":         0,
-		// 	"haworkshopyc1/sensor/powermeteryc1/state": 0,
-		// 	"haworkshopyc1/sensor/solaryc1/state":      0}
-		// if token := c.SubscribeMultiple(topics, onMessageReceived); token.Wait() && token.Error() != nil {
-		// 	//if token := c.Subscribe(*topic, byte(*qos), onMessageReceived); token.Wait() && token.Error() != nil {
-		// 	panic(token.Error())
-		// }
-
-		// for _, rig := range rigs {
-		// 	json_string, err := rig.GetStat()
-		// 	if err != nil {
-		// 		log.Println(err)
-		// 	} else {
-		// 		rig.PublishConfig(c, json_string)
-		// 	}
-		// }
-
 		mqtt_status = 1
 	}
 	connOpts.OnConnectionLost = func(c mqtt.Client, err error) {
@@ -123,7 +79,6 @@ func init_mqtt() mqtt.Client {
 }
 
 func init() {
-	log.SetFlags(0)
 }
 
 func main() {
